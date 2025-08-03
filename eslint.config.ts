d5d1042bab4702d83ec998cd0411778c6af34c8e
othmantabeche <othmantabeche@gmail.com>
2025-08-03 20:42:02 +0200

    Remove ESLint configuration and related files; update dependencies in package.json and package-lock.json; refactor App component and main entry point; delete unused CSS and SVG assets.

---
+++ eslint.config.ts
@@ -0,0 +1,40 @@
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+
+export default tseslint.config([
+  {
+    ignores: ['dist/**', 'eslint.config.ts'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      // TypeScript ESLint configs
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
+    },
+    rules: {
+      // React rules
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]);
