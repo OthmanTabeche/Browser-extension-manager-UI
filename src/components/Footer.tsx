559fda0622b800031ef3d0af9e75a3bdf06d5a2a
othmantabeche <othmantabeche@gmail.com>
2025-08-05 13:06:45 +0200

    Update Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {type JSX} from "react"
 const Footer = (): JSX.Element => {
     return (
         <footer className="flex justify-center items-center m-12">
-            <h3>Made with ❤️ by <a href="https://github.com/OthmanTabeche" className="underline">Othman</a></h3>
+            <h3>Made by <a href="https://github.com/OthmanTabeche" className="underline">Othman</a></h3>
         </footer>
     )
 }
